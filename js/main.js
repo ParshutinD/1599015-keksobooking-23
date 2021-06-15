ecb6f697f9ef26e77bf929441f110c8f88198947
Паршутин Даниил <daniilparshutin1@gmail.com>
2021-06-15 23:34:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -32,3 +32,49 @@ function getRandomFloat(min, max, floatPoint) {
 }
 
 getRandomFloat();
+
+// Генерация данных
+let xx = getRandomInt(1,10);
+
+if(xx < 10) {
+  xx = '0' + xx;
+}
+
+const author = {
+  avatar: `img/avatars/user{${xx}}.png`,
+};
+
+const typeList = ['palace','flat','house','bungalow','hotel'];
+const checkinList = ['12:00','13:00','14:00'];
+const checkoutList = ['12:00','13:00','14:00'];
+const featuresList = _.sampleSize(['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'], getRandomInt(1,6)) ;
+const photosList = _.sampleSize(['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],getRandomInt(1,3));
+
+const location = {
+  lat:getRandomFloat(35.65000, 35.70000, 5),
+  lng:getRandomFloat(139.70000, 139.80000, 5),
+};
+
+const offer = {
+  title: 'Информация о жилье',
+  address: `{${location.lat}}, {${location.lng}}`,
+  price: getRandomInt(1, 999999),
+  type: typeList[getRandomInt(0,4)],
+  rooms: getRandomInt(1, 999999),
+  guests: getRandomInt(1, 999999),
+  checkin: checkinList[getRandomInt(0,2)],
+  checkout: checkoutList[getRandomInt(0,2)],
+  features: featuresList,
+  description:'Уютно',
+  photos:photosList,
+};
+
+
+const adDescription = [];
+
+function createNewArray () {
+  adDescription.push(author,location,offer);
+}
+
+createNewArray();
+
